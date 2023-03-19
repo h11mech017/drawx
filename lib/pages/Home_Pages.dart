0d85a4ade08ffdbee3e1f4a99a82580a947b5833
LeoWong <o0siu123210o@icloud.com>
2023-03-19 16:31:45 +0800

    Changed UI Config

---
+++ lib/pages/Home_Pages.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class HomePages extends StatefulWidget {
+  const HomePages({Key? key}) : super(key: key);
+
+  @override
+  State<HomePages> createState() => _HomePagesState();
+}
+
+class _HomePagesState extends State<HomePages> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "DrawX_",
+      home: Scaffold(
+        appBar: AppBar(title: const Text("DrawX_"),),
+        body: Center(
+          child: Column(
+            children: [
+              const Spacer(),
+              const Draw(),
+              const Spacer(),
+              const TextField(
+                obscureText: false,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Name",
+                ),
+              ),
+              TextButton(
+                onPressed: () { },
+                child: const Text('Add'),
+              ),
+              const Spacer()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Draw extends StatelessWidget {
+  const Draw({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 400,
+      height: 400,
+      // color: Colors.brown,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(360),
+          color: Colors.blue[100]
+      ),
+
+      child: TextButton(
+        onPressed: () {  },
+        child: const Text("Roll!"),
+      ),
+    );
+  }
+}
+
