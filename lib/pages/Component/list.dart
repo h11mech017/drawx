4ba227f376ca0a1b8d6b855ef65da5f1b86f768a
Harris404 <1149635813@qq.com>
2023-04-25 21:45:15 -0700

    add the delete function of the result

--- lib/pages/Component/list.dart
+++ lib/pages/Component/list.dart
@@ -1,10 +1,29 @@
 // pages/Component/urls.dart
 import 'package:flutter/material.dart';
 
-class ListPages extends StatelessWidget {
+class ListPages extends StatefulWidget {
   final List<String> results; // Add a results parameter
 
-  const ListPages({Key? key, required this.results}) : super(key: key);
+  const ListPages({Kggogey? key, required this.results}) : super(key: key);
+
+  @override
+  _ListPagesState createState() => _ListPagesState();
+}
+
+class _ListPagesState extends State<ListPages>{
+    List<String> _results = [];
+
+    @override
+    void initState(){
+      super.initState();
+      _results = widget.results;
+    }
+
+    void _removeResult(int index){
+      setState((){
+        _results.removeAt(index);
+      });
+    }
 
   @override
   Widget build(BuildContext context) {
@@ -13,14 +32,14 @@ class ListPages extends StatelessWidget {
         title: const Text('Results'),
       ),
       body: ListView.builder(
-        itemCount: results.length,
+        itemCount: _results.length,
         itemBuilder: (context, index) {
           return ListTile(
-            title: Text(results[index]),
+            title: Text(_results[index]),
             trailing: IconButton(
               icon: const Icon(Icons.delete),
               onPressed: () {
-                // Remove the result from the list and update the UI
+                _removeResult(index);// Remove the result from the list and update the UI
               },
             ),
           );
